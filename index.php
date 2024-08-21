2c0803503b64e7fef900a73b6f95f2ca822e7146
Agbortoko <arreyagbortoko@gmail.com>
2024-08-21 16:07:25 +0100

    Corrections instructure of boilerplate implemented

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php require_once __DIR__ . "/vendor/autoload.php" ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Page</title>
+    <?php require_once basePath('includes/vite.php'); ?>
+</head>
+<body>
+    
+</body>
+</html>
